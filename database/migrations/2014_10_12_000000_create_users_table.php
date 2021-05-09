484b5afca5e15e8801f9b58a775bbab0ec9f63db
امین فراست <amin.ferasat@gmail.com>
2021-05-09 05:44:24 +0430

    اصلاح شد

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
@@ -15,13 +16,32 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable();
+            $table->string('family')->nullable();
+            $table->enum('status' , ['Active' , 'deActive'])->default('deActive');
+            $table->string('role')->nullable()->default('customer'); // contributor / editor / Admin /
+            $table->string('username')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('gender')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
+
+        DB::table('users')->insert([
+            'name' => 'admin' ,
+            'email' => 'admin@tarahsite.net' ,
+            'phone' => '9372088771' ,
+            'password' => bcrypt('@dmiNBloger' ),
+        ]);
+        DB::table('users')->insert([
+            'name' => 'user1' ,
+            'email' => 'user1@tarahsite.net' ,
+            'phone' => '9372088771' ,
+            'password' => bcrypt('159951@@' ),
+        ]);
     }
 
     /**
